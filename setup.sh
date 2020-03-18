536c64ee41314010d61b842259d5a8dc799c2fe7
Dz@shell <36556358+termux4me@users.noreply.github.com>
2020-03-18 17:59:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-unzip Rs.zip
+#!/bin/bash
+#setup.sh
 pkg update && pkg upgrade
+pkg install zip
+unzip Rs.zip
 pkg install python
 cp -rf Rsettings ~/../usr/bin
 chmod +x ~/../usr/bin/Rsettings
-rm Rs.zip
+echo "."
+echo ".."
+echo "..."
+echo "now you can type Rsettings to start"
